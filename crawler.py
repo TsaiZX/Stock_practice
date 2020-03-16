1b8b95920a2502779decd63a4a8154a004ef3397
TsaiZX <p154968@gmail.com>
2020-03-16 15:13:58 +0800

    need to update

--- crawler.py
+++ crawler.py
@@ -12,9 +12,9 @@ getRequest = requests.post('https://www.twse.com.tw/exchangeReport/MI_INDEX?resp
 
 
 # parse request to dataframe
-stock_df = pd.read_csv(StringIO(getRequest.text.replace("=", "")), header=["證券代號" in l for l in getRequest.text.split("\n")].index(True)-1)
+stock_df = pd.read_csv(StringIO(getRequest.text.replace("=", "")) ,header=["證券代號" in l for l in getRequest.text.split("\n")].index(True)-1)
 
 # store
-stock_df.to_csv('./data/%s.csv'%(Date) , index = False)
+stock_df.to_csv('./data/%s.csv'%(Date) ,index = False ,encoding="utf_8_sig")
 
 
