f7e9af981baa238fd1d07153905c74a6ff6da5d6
TsaiZX <p154968@gmail.com>
2020-03-23 14:43:27 +0800

    update

--- crawler.py
+++ crawler.py
@@ -1,20 +1,27 @@
 import requests
 import pandas as pd
-import numpy as np
+import logging
 from io import StringIO
 
 
+def parseRequest(request):
+    # parse request to dataframe
+    stock_df = pd.read_csv(StringIO(request.text.replace("=", "")) ,header=["證券代號" in l for l in request.text.split("\n")].index(True)-1)
+    return stock_df
 
+def getRequest(Date):
+    # get request
+    requestOutput = requests.post('https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + Date + '&type=ALL')
+    return parseRequest(requestOutput)
 
-# get request
-Date = '20200316'
-getRequest = requests.post('https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + Date + '&type=ALL')
+if __name__ == '__main__':
 
+    
+    Date = '20200316'
+    stockDf = getRequest(Date)
+    
 
-# parse request to dataframe
-stock_df = pd.read_csv(StringIO(getRequest.text.replace("=", "")) ,header=["證券代號" in l for l in getRequest.text.split("\n")].index(True)-1)
-
-# store
-stock_df.to_csv('./data/%s.csv'%(Date) ,index = False ,encoding="utf_8_sig")
+    # store
+    stockDf.to_csv('./data/%s.csv'%(Date) ,index = False ,encoding="utf_8_sig")
 
 
