f165234ac9c805b5e9abd00c78f5a21e31d63409
Dion Wu <dionwu@DiondeMacBook-Pro.local>
2020-03-26 21:39:32 +0800

    :

--- crawler.py
+++ crawler.py
@@ -1,7 +1,10 @@
 import requests
 import pandas as pd
 import logging
+import time
+import datetime
 from io import StringIO
+from tqdm import tqdm
 
 
 def parseRequest(request):
@@ -14,14 +17,47 @@ def getRequest(Date):
     requestOutput = requests.post('https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + Date + '&type=ALL')
     return parseRequest(requestOutput)
 
+def getTodayDate():
+    # get today date
+    return time.strftime('%Y%m%d', time.localtime())
+    
+def getLastStockDate():
+    # get last stock date from log
+    logFile = open('stockLog.log')
+    listOfLog = logFile.readlines()
+    listOfLog.reverse()
+    lastList = listOfLog[0] 
+    lastDate = lastList.split(' ')
+    lastDate.reverse()
+    return lastDate[0]
+
+def stringToDatetime(string):
+    return datetime.datetime.strptime(string,'%Y%m%d').date()
+    
+def datetimeToString(datetime):
+    return datetime.strftime('%Y%m%d')
+
+
+FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+logging.basicConfig(level=logging.INFO, filename='stockLog.log', filemode='a', format=FORMAT)
+
 if __name__ == '__main__':
 
-    
-    Date = '20200316'
-    stockDf = getRequest(Date)
-    
+    begin = stringToDatetime(getLastStockDate())
+    end = stringToDatetime(getTodayDate())
 
-    # store
-    stockDf.to_csv('./data/%s.csv'%(Date) ,index = False ,encoding="utf_8_sig")
+    print('begin:%s'%(begin))
+    print('end:  %s'%(end))
 
+    for i in tqdm(range((end - begin).days)):
+        day = begin + datetime.timedelta(days = i)
+        Date = datetimeToString(day)
 
+        try:
+            stockDf = getRequest(Date)
+            logging.info('stockDate %s',Date)
+
+            # store
+            stockDf.to_csv('./data/%s.csv'%(Date) ,index = False ,encoding="utf_8_sig")
+        except:
+            print('%s Stock rest or error'%(Date))
